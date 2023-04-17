fab2faf53ff6f8e29e4add51364aa750f724edc4
Bitlime <contact@lorenzosuffritti.com>
2023-04-18 00:09:03 +0200

    Init

---
+++ src/beeptalk-widget.php
@@ -0,0 +1,107 @@
+<?php
+/*
+Plugin Name: Beeptalk Widget
+Plugin URI: https://beeptalk.app/wp-plugin
+Description: Support chatbot service powered by ChatGPT natural language processing AI, bring your customer service to the next level and provide automatic human-like support to your clients.
+Author: Beeptalk
+Author URI: https://beeptalk.app/
+Version: 1.0.0
+License: GPL-3.0
+License URI:  https://www.gnu.org/licenses/gpl-3.0.html
+*/
+
+// Add the plugin settings page
+add_action('admin_menu', 'beeptalk_widget_settings_page');
+function beeptalk_widget_settings_page() {
+    add_menu_page(
+        'Beeptalk Widget Settings',
+        'Beeptalk Widget Settings',
+        'manage_options',
+        'beeptalk-widget-settings',
+        'beeptalk_widget_settings_page_callback'
+    );
+}
+
+// Register the plugin settings
+add_action('admin_init', 'register_beeptalk_widget_settings');
+function register_beeptalk_widget_settings() {
+    register_setting('beeptalk-widget-settings-group', 'beeptalk_project_id');
+    register_setting('beeptalk-widget-settings-group', 'use_dark_icon');
+    register_setting('beeptalk-widget-settings-group', 'primary_color');
+    register_setting('beeptalk-widget-settings-group', 'beeptalk_agent');
+}
+
+// Callback function for the plugin settings page
+function beeptalk_widget_settings_page_callback() {
+    ?>
+    <div class="wrap">
+        <h1>Beeptalk Widget Settings</h1>
+        <form method="post" action="options.php">
+            <?php settings_fields('beeptalk-widget-settings-group'); ?>
+            <?php do_settings_sections('beeptalk-widget-settings-group'); ?>
+            <table class="form-table">
+                <tr valign="top">
+                    <th scope="row">Beeptalk project ID *</th>
+                    <td><input type="text" name="beeptalk_project_id" value="<?php echo esc_attr(get_option('beeptalk_project_id')); ?>" required /></td>
+                </tr>
+                <tr valign="top">
+                    <th scope="row">Use dark Icon</th>
+                    <td><input type="checkbox" name="use_dark_icon" <?php checked( get_option('use_dark_icon'), 'on' ); ?> /></td>
+                </tr>
+                <tr valign="top">
+                    <th scope="row">Primary Color</th>
+                    <td><input type="color" name="primary_color" value="<?php echo esc_attr(get_option('primary_color')); ?>" /></td>
+                </tr>
+                <tr valign="top">
+                    <th scope="row">Force agent (by email)</th>
+                    <td><input type="text" name="beeptalk_agent" value="<?php echo esc_attr(get_option('beeptalk_agent')); ?>" /></td>
+                </tr>
+            </table>
+            <?php submit_button(); ?>
+        </form>
+    </div>
+    <?php
+}
+
+// Insert the custom script
+function insert_beeptalk_widget_script() {
+    $id = get_option('beeptalk_project_id');
+    if (!empty($id)) {
+        $user = wp_get_current_user();
+        $user_first_name = $user->first_name;
+        $uuid = get_current_user_id();
+        $suid = NULL;
+        if (isset($_COOKIE['wordpress_logged_in_'])) {
+            $cookie = $_COOKIE['wordpress_logged_in_'];
+            $suid = substr($cookie, 0, 64);
+        }
+        $dark_icon = get_option('use_dark_icon') ? 'true' : 'false';
+        $primary_color = get_option('primary_color') ? get_option('primary_color') : '';
+        $agent = get_option('beeptalk_agent');
+        
+        echo '<script src="https://cdn.jsdelivr.net/gh/beeptalk-app/beeptalk-widget@latest/index.min.js"></script>';
+        echo '<script>';
+        echo 'beeptalkInit({';
+        echo "id: '$id',";
+        if (!empty($uuid) && !is_null($uuid) && isset($uuid)) {
+            echo "uuid: '$uuid',";
+        }
+        if (!empty($suid) && !is_null($suid) && isset($suid)) {
+            echo "suid: '$suid',";
+        }
+        if (!empty($user_first_name) && !is_null($user_first_name) && isset($user_first_name)) {
+            echo "uname: '$user_first_name',";
+        }
+        if (!empty($dark_icon) && !is_null($dark_icon) && isset($dark_icon)) {
+            echo "darkIcon: $dark_icon,";
+        }
+        echo "primaryColor: '$primary_color',";
+        if (!empty($agent) && !is_null($agent) && isset($agent)) {
+            echo "agent: '$agent',";
+        }
+        echo '});';
+        echo '</script>';
+    }
+}
+add_action( 'wp_footer', 'insert_beeptalk_widget_script' );
+?>
---
+++ src/readme.txt
@@ -0,0 +1,39 @@
+=== Your Plugin Name ===
+Contributors: beeptalk
+Tags: beeptalk,chatbot,widget,ai,chatgpt,support,customer service,chat
+Requires at least: 6.0
+Tested up to: 6.1
+Stable tag: 1.0
+License: GPLv3 or later
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ 
+Beeptalk official wordpress plugin.
+ 
+== Description ==
+ 
+Support chatbot service powered by ChatGPT natural language processing AI, bring your customer service to the next level and provide automatic human-like support to your clients.
+ 
+== Installation ==
+ 
+1. Install the plugin.
+2. Go to the **Plugins** page and activate the plugin.
+3. Go to the Beeptalk widget plugin settings.
+4. Copy and paste your Beeptalk project ID (from https://dashboard.beeptalk.app) on the "Beeptalk project ID" field.
+5. Set the other other fields as you wish (optional)
+6. You are done!
+ 
+== Frequently Asked Questions ==
+ 
+= How do I use this plugin? =
+ 
+You only need to fill the plugin settings on "Beeptalk Widget Settings" with the Beeptalk project ID (mandatory) and the other parameters (optional), and you are done, the widget will show up on your webiste.
+ 
+= How to uninstall the plugin? =
+ 
+Simply deactivate and delete the plugin. 
+
+== Screenshots ==
+
+== Changelog ==
+= 1.0.0 =
+* Plugin released. 
